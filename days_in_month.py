6ebd048565c6f0a81f8c8e9f1e7814fd9916b55a
haileabZ <138156036+haileabZ@users.noreply.github.com>
2023-11-21 21:16:18 +0300

    Update days_in_month.py

--- days_in_month.py
+++ days_in_month.py
@@ -1,3 +1,5 @@
+
+
 def isleap(year):
     if (year % 400 == 0) or  ((year % 100 != 0) and  (year % 4 == 0)):
         return True
@@ -9,6 +11,6 @@ def day_finder(year,month):
         days[1]=29
     print("number of days: ",days[month-1])
 
-month=int(input("enter the month:"))
-year=int(input("enter the year:"))
+month=int(input("enter the month in number:[1,2,3,4,5,6,7,8,9,10,11,12]").strip())
+year=int(input("enter the year:").strip())
 day_finder(year,month)

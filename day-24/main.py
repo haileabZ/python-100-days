35ad7ba0b284a26c55269c43711a65c1a801c581
haileabZ <138156036+haileabZ@users.noreply.github.com>
2024-01-02 07:30:22 +0300

    Add files via upload

---
+++ day-24/main.py
@@ -0,0 +1,18 @@
+
+text = """ Dear [name]
+
+ you are invited to my birthday this monday.
+
+ Hope you can make it!
+
+ Seni"""
+
+with open("input/names/invited_names.txt", "r") as file:
+     names = file.readlines()
+
+for line in names:
+     name = line.rstrip().strip()
+     letter = text.replace("[name]", name)
+     with open(f"output/readyToSend/letter_for_{name}.txt","w") as f:
+         f.write(letter)
+

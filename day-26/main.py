54f6f6b047e1b220e7b772be02287ecca6017686
haileabZ <138156036+haileabZ@users.noreply.github.com>
2024-01-04 05:00:26 +0300

    Create main.py

---
+++ day-26/main.py
@@ -0,0 +1,19 @@
+
+from pandas import read_csv
+
+name = input("enter your name").upper()
+letters_in_name = [letter for letter in name]
+
+data = read_csv("nato_phonetic_alphabet.csv")
+codes = data["code"].tolist()
+all_letters = data["letter"].tolist()
+
+# finding dictionary for of csv file
+
+dict_form = {all_letters[i]: codes[i] for i in range(26)}
+
+# creating alphaphonetic words
+
+letter_word_join = [dict_form[letter] for letter in letters_in_name ]
+
+print(letter_word_join)

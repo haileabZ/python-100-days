90184255852fceadfae8f50bb88b04309f74fa79
Haileab Zinabu <138156036+haileabZ@users.noreply.github.com>
2024-01-15 14:04:43 +0300

    Update main.py

--- monday-motivation-sender/main.py
+++ monday-motivation-sender/main.py
@@ -1,1 +1,22 @@
 
+
+import smtplib
+import datetime as dt
+from random import choice
+
+MY_EMAIL = "etest8915@gmail.com"
+MY_PASSWORD = "yasw zogp trvv yluf"
+
+today = dt.datetime.now().weekday()
+
+if today == 0:
+    with open("quotes.txt") as txt_file:
+        all_quotes = txt_file.readlines()
+        quote = choice(all_quotes)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject:Sending motivational "
+                                                                       f"quote every monday\n\n{quote}")
+
+

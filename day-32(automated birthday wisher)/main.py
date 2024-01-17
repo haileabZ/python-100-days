bfd48455935c7b81966f4dfd4fca759e2ebe7919
Haileab Zinabu <138156036+haileabZ@users.noreply.github.com>
2024-01-17 05:13:47 +0300

    Add files via upload

---
+++ day-32(automated birthday wisher)/main.py
@@ -0,0 +1,43 @@
+
+##################### Extra Hard Starting Project ######################
+
+import smtplib
+import datetime as dt
+import pandas as pd
+from random import choice
+import smtplib
+
+MY_PASSOWRD = "feda ovjj wpge fiku"
+MY_EMAIL = "etest8915@gmail.com"
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+
+today = dt.datetime.now()
+year = today.year
+month = today.month
+day = today.day
+
+file = pd.read_csv("birthdays.csv").to_dict(orient="records")
+
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+for d in file:
+    if d["year"] == year and d["month"] == month and d["day"] == day:
+        rand_letter = choice(["letter_1.txt", "Letter_2.txt", "letter_3.txt"])
+        with open(f"letter_templates/{rand_letter}", "r") as f:
+            text_to_send = f.read()
+            last_letter = text_to_send.replace("[NAME]", d["name"])
+            email_of_person = d["email"]
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSOWRD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=email_of_person,
+                                msg=f"Subject: My heartfelt birthday wish\n\n{last_letter}")
+
+
+
+

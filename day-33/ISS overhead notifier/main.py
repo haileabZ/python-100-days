1d271aaeb3f2af30e288b0b9f191ff9f14864dda
Haileab Zinabu <138156036+haileabZ@users.noreply.github.com>
2024-01-24 04:16:50 +0300

    Update main.py

--- day-33/ISS overhead notifier/main.py
+++ day-33/ISS overhead notifier/main.py
@@ -19,6 +19,7 @@ def is_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
+    
 
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])

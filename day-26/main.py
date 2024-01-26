2bd55d3a7e50b88f890f6695c8e28de8e2601905
Haileab Zinabu <138156036+haileabZ@users.noreply.github.com>
2024-01-26 16:11:59 +0300

    Update main.py

--- day-26/main.py
+++ day-26/main.py
@@ -17,7 +17,6 @@ dict_form = {all_letters[i]: codes[i] for i in range(26)}
 
 # creating alphaphonetic words
 
-
 letter_word_join = [dict_form[letter] for letter in letters_in_name ]
 
 print(letter_word_join)

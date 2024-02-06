e42905d51ad2cf7341d7acef79b2f66a1bdf41de
Haileab Zinabu <138156036+haileabZ@users.noreply.github.com>
2024-02-06 20:59:30 +0300

    Update main.py

--- day-29/password-generator/main.py
+++ day-29/password-generator/main.py
@@ -48,7 +48,8 @@ def save():
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
                 email_entry.delete(0, END)
-                
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 

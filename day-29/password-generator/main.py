fdd528a2ce430cd657c1cb5114c1538c6ab89762
Haileab Zinabu <138156036+haileabZ@users.noreply.github.com>
2024-02-06 20:58:57 +0300

    Update main.py

--- day-29/password-generator/main.py
+++ day-29/password-generator/main.py
@@ -8,7 +8,6 @@ import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-
 def generate():
 
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
